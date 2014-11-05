21a05eca45d18ceeaefee319708cd5f836f51dd6
Calvin Beck <hobbes@ualberta.ca>
2014-11-05 16:51:24 -0700

    Initial nix shell setup.

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? (import <nixpkgs> {})
+, haskellPackages ? pkgs.haskellPackages_ghc783
+}:
+
+haskellPackages.cabal.mkDerivation (self: {
+  pname = "FeatureSquish";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  buildDepends = with haskellPackages; [ ];
+  buildTools = with haskellPackages; [ cabalInstall ];
+  meta = {
+    description = "Time the speed of a serial connection with an Arduino.";
+    license = self.stdenv.lib.licenses.mit;
+    platforms = self.ghc.meta.platforms;
+  };
+})
