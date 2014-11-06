5b65f5d798a055479957a21365e0e1e1e5bde7f6
Calvin Beck <hobbes@ualberta.ca>
2014-11-05 21:27:45 -0700

    Feature removal added. Cleanup. Other fun stuff.

--- Main.hs
+++ Main.hs
@@ -21,8 +21,10 @@
    SOFTWARE.
 -}
 
-import Data.List.Split
+import Data.List
+import Data.List.Split hiding (split)
 import System.Environment
+import System.Random
 
 -- | Type for PSSP data from http://pssp.srv.ualberta.ca/.
 data InputLine = InputLine { time :: Double  -- ^ Time of event, or censoring.
@@ -31,12 +33,24 @@ data InputLine = InputLine { time :: Double  -- ^ Time of event, or censoring.
                            }
 
 instance Show InputLine where
-  show inp = t ++ " " ++ " " ++ c ++ f
+  show inp = t ++ " " ++ c ++ " " ++ f
        where t = show $ time inp
              c = if censored inp then "1" else "0"
-             f = show $ features inp
+             f = concat . intersperse " " . map (\(f,v) -> (show f) ++ ":" ++ (show v)) $ features inp
 
--- | Read a line from the http://pssp.srv.ualberta.ca/ dataset.
+
+main :: IO ()
+main = do [file, probStr] <- getArgs
+          let prob = read probStr
+          fileData <- readFile file
+          gen <- getStdGen
+          (print . squishList prob gen . inputsRead) fileData
+
+-- | Read an entire dataset from http://pssp.srv.ualberta.ca/
+inputsRead :: String -> [InputLine]
+inputsRead = map lineRead . lines
+
+-- | Read a single line from the http://pssp.srv.ualberta.ca/ dataset.
 lineRead :: String -> InputLine
 lineRead str = InputLine (read t) (if read c == 0 then False else True) features
          where (t:c:featureStrs) = words str
@@ -44,7 +58,17 @@ lineRead str = InputLine (read t) (if read c == 0 then False else True) features
                readFeature f = (read number, read value)
                  where [number, value] = splitOn ":" f
 
-main :: IO ()
-main = do [file] <- getArgs
-          fileData <- readFile file
-          (print . map lineRead . lines) fileData
+-- | Remove features with a given probability from an InputLine list.
+squishList :: RandomGen g => Double -> g -> [InputLine] -> [InputLine]
+squishList prob gen inps = map (\(g, i) -> squish prob g i) (zip (splits gen) inps)
+
+-- | Remove features with a given probability from an InputLine.
+squish :: RandomGen g => Double -> g -> InputLine -> InputLine
+squish prob gen inp = inp {features = [f | (x, f) <- zip missing feats, x == 1]}
+       where missing = map (\x -> if x > prob then 1 else 0) (randomRs (0.0, 1.0) gen)
+             feats = features inp
+
+-- | Create an infinite list of random generators.
+splits :: RandomGen a => a -> [a]
+splits g = gh:(splits gt)
+       where (gh, gt) = split g
