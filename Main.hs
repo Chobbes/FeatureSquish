61af5d1b1f4f87dd3c33b29a5d9a65a35036ab9e
Calvin Beck <hobbes@ualberta.ca>
2014-11-05 22:11:30 -0700

    Beautification.

--- Main.hs
+++ Main.hs
@@ -36,7 +36,7 @@ instance Show InputLine where
   show inp = t ++ " " ++ c ++ " " ++ f
        where t = show $ time inp
              c = if censored inp then "1" else "0"
-             f = concat . intersperse " " . map (\(f,v) -> (show f) ++ ":" ++ (show v)) $ features inp
+             f = unwords . map (\(f,v) -> show f ++ ":" ++ show v) $ features inp
 
 
 main :: IO ()
@@ -44,7 +44,7 @@ main = do [file, probStr, outFile] <- getArgs
           let prob = read probStr
           fileData <- readFile file
           gen <- getStdGen
-          let newData = (concat . intersperse "\n" . map show . squishList prob gen . inputsRead) fileData
+          let newData = (intercalate "\n" . map show . squishList prob gen . inputsRead) fileData
           writeFile outFile newData
 
 -- | Read an entire dataset from http://pssp.srv.ualberta.ca/
@@ -53,7 +53,7 @@ inputsRead = map lineRead . lines
 
 -- | Read a single line from the http://pssp.srv.ualberta.ca/ dataset.
 lineRead :: String -> InputLine
-lineRead str = InputLine (read t) (if read c == 0 then False else True) features
+lineRead str = InputLine (read t) (read c /= 0) features
          where (t:c:featureStrs) = words str
                features = map readFeature featureStrs
                readFeature f = (read number, read value)
@@ -61,7 +61,7 @@ lineRead str = InputLine (read t) (if read c == 0 then False else True) features
 
 -- | Remove features with a given probability from an InputLine list.
 squishList :: RandomGen g => Double -> g -> [InputLine] -> [InputLine]
-squishList prob gen inps = map (\(g, i) -> squish prob g i) (zip (splits gen) inps)
+squishList prob gen = zipWith (squish prob) (splits gen)
 
 -- | Remove features with a given probability from an InputLine.
 squish :: RandomGen g => Double -> g -> InputLine -> InputLine
@@ -71,5 +71,5 @@ squish prob gen inp = inp {features = [f | (x, f) <- zip missing feats, x == 1]}
 
 -- | Create an infinite list of random generators.
 splits :: RandomGen a => a -> [a]
-splits g = gh:(splits gt)
+splits g = gh : splits gt
        where (gh, gt) = split g
