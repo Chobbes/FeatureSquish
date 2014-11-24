8885584a202de4b43c62e3056d5105b197b8f196
Calvin Beck <hobbes@ualberta.ca>
2014-11-23 23:49:29 -0700

    Adding attoparsec as a dependency.

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ haskellPackages.cabal.mkDerivation (self: {
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  buildDepends = with haskellPackages; [ split random ];
+  buildDepends = with haskellPackages; [ split random attoparsec ];
   buildTools = with haskellPackages; [ cabalInstall ];
   meta = {
     description = "Time the speed of a serial connection with an Arduino.";
